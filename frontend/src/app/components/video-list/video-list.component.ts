1447f2915f27b299e7200b913a1a454e2668bd92
ismail <i.moukafih@gmail.com>
2023-03-24 10:10:44 +0100

    init

--- frontend/src/app/components/video-list/video-list.component.ts
+++ frontend/src/app/components/video-list/video-list.component.ts
@@ -1,5 +1,5 @@
 import {
-  Component,
+  Component, HostListener,
   OnInit
 } from '@angular/core';
 import {ChannelDataService} from "../../services/channel.data-service";
@@ -14,6 +14,40 @@ import {VideoService} from "../../services/video.service";
 export class VideoListComponent implements OnInit {
   list: Channel[] = [];
   activeChannel!: Channel;
+  public showList = false;
+
+  @HostListener('document:keydown', ['$event'])
+  onKeydownHandler(event: any) {
+    console.log('onKeydownHandler', event.keyCode);
+    if (event.keyCode == 13){
+      console.log('onKeydownHandler', 'enter');
+      this.showList = false;
+    }
+    if (event.keyCode == 38){
+      console.log('onKeydownHandler', 'keyUp');
+      this.showList = true;
+    }
+    if (event.keyCode == 40){
+      console.log('onKeydownHandler', 'keyDown');
+      this.showList = true;
+    }
+    if (event.keyCode == 37){
+      console.log('onKeydownHandler', 'keyLeft');
+      this.showList = true;
+    }
+    if (event.keyCode == 39){
+      console.log('onKeydownHandler', 'keyRight');
+      this.showList = true;
+    }
+    if (event.keyCode == 49){
+      console.log('onKeydownHandler', '1');
+      this.playIt(this.list[0]);
+    }
+    if (event.keyCode == 50){
+      console.log('onKeydownHandler', '2');
+      this.playIt(this.list[1]);
+    }
+  }
 
   constructor(
     private channelDataService: ChannelDataService,
@@ -24,11 +58,12 @@ export class VideoListComponent implements OnInit {
     this.channelDataService.all()
       .subscribe((ELEMENT_DATA) => {
         this.list = ELEMENT_DATA
-        this.playIt(ELEMENT_DATA[4]);
+        this.playIt(this.list[4]);
       });
   }
 
   public playIt(channel: Channel): void {
+
     this.videoService.setCurrentVideoByIndex(channel);
     this.videoService.play();
     this.activeChannel = channel;
